1d710d02144c788a9e279d59ca6ca568e462d86b
Sol Amour <amoursol@gmail.com>
2018-07-18 15:28:28 +0100

    Update filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py

--- revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
+++ revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
@@ -13,14 +13,18 @@ import RevitServices # Importing RevitServices
 from RevitServices.Persistence import DocumentManager # From RevitServices import the Document Manager
 clr.AddReference("RevitAPI") # Adding the RevitAPI.dll module to access the Revit API
 import Autodesk # Here we import the Autodesk namespace
-# From the Autodesk namespace - derived down to the Revit Database, we import only the Filtered Element Collector and BuiltInCategory classes
+# From the Autodesk namespace - derived down to the Revit Database, we import only the Filtered Element 
+# Collector and BuiltInCategory classes
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, FamilyInstance
 
-# Here we give the Revit Document a nickname of 'doc' which allows us to simply call 'doc' later without having to type the long namespace name 
+# Here we give the Revit Document a nickname of 'doc' which allows us to simply call 'doc' later without 
+# having to type the long namespace name 
 doc = DocumentManager.Instance.CurrentDBDocument
 
-# We create a 'Filtered Element Collector' that will collect, in the 'Active View' of our document, all 'Family Instances' of a particular category ('Planting'). We then cast it to Elements so we can use it in Dynamo.
-builtInCollector = FilteredElementCollector( doc, doc.ActiveView.Id ).OfClass( FamilyInstance ).OfCategory( BuiltInCategory.OST_Planting ).WhereElementIsNotElementType().ToElements()
+# We create a 'Filtered Element Collector' that will collect, in the 'Active View' of our document, all 
+# 'Family Instances' of a particular category ('Planting'). We then cast it to Elements so we can use it in Dynamo.
+builtInCollector = FilteredElementCollector( doc, doc.ActiveView.Id ).OfClass( FamilyInstance )
+.OfCategory( BuiltInCategory.OST_Planting ).WhereElementIsNotElementType().ToElements()
 
 # To get our results back inside of Dynamo, we need to append a list to the OUT port
 OUT = builtInCollector
