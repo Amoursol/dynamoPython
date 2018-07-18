d00a695808863659157567e3df2e2526a2bbaebf
Sol Amour <amoursol@gmail.com>
2018-07-18 15:20:53 +0100

    Update listTransposeListComprehension.py

--- concepts/listTransposeListComprehension.py
+++ concepts/listTransposeListComprehension.py
@@ -21,5 +21,11 @@ import clr
 numbers = [ 1, 2, 3, 4, 5 ]
 alphabet = [ "A", "B", "C", "D", "E" ]
 
-# Here we create a list of the zip function being called on our matrix. We need to prefix the list with an asterisk ( * ) which tells our zip function to unpack (Remove the outer list) of our matrix. Without this we would be calling zip( [ [0,1,2,3,4], ['a','b','c','d','e'] ] ) which will cause zip to fail. If we utilise the asterisk, it unpacks the outer list resulting in: zip( [0,1,2,3,4], ['a','b','c','d','e'] ) which will correctly execute
+# Here we create a list of the zip function being called on our matrix. 
+# We need to prefix the list with an asterisk ( * ) which tells our zip 
+# function to unpack (Remove the outer list) of our matrix. Without this 
+# we would be calling zip( [ [0,1,2,3,4], ['a','b','c','d','e'] ] ) which 
+# will cause zip to fail. If we utilise the asterisk, it unpacks the outer 
+# list resulting in: zip( [0,1,2,3,4], ['a','b','c','d','e'] ) which will 
+# correctly execute
 OUT = [ [x, y] for x,y in zip(numbers, alphabet) ]
