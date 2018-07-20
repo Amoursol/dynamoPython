75ca8813ba33aab5cb3aa8077fd6d20bbd614ec7
Sol Amour <amoursol@gmail.com>
2018-07-20 09:28:05 +0100

    Update sortByIndexValueReversed.py

--- workflow/sortByIndexValueReversed.py
+++ workflow/sortByIndexValueReversed.py
@@ -11,7 +11,8 @@ __version__ = '1.0.0'
 # Reverse = Choice to reverse the sorting ( Optional )
 
 # NOTES: 
-# To utilise either the 'key' or 'reverse' optional parameters, you must use the correct syntax of: key = x, or reverse = True
+# To utilise either the 'key' or 'reverse' optional parameters, 
+# you must use the correct syntax of: key = x, or reverse = True
 # Key can be a user defined function with which to sort
 
 
@@ -21,5 +22,7 @@ import operator
 # The input port
 plots = IN[0]
 
-# Sorting our output list using the itemgetter operator which simply states sort by indices in this order: Index 1 first, then index 2 second, then follow on with natural sorting afterwards
+# Sorting our output list using the itemgetter operator which simply 
+# states sort by indices in this order: Index 1 first, then index 2 
+# second, then follow on with natural sorting afterwards
 OUT = sorted( plots, key = operator.itemgetter( 1, 2 ), reverse = True )
