d1b192b6f1a73249dbd62964f7c6a57ae26f5242
Sol Amour <amoursol@gmail.com>
2018-07-20 22:12:12 +0100

    Update listGetItemAtIndex.py

--- concepts/listGetItemAtIndex.py
+++ concepts/listGetItemAtIndex.py
@@ -11,14 +11,16 @@ __version__ = '1.0.0'
 # NOTES: 
 # All parameters have to be integers and can be positive or negative
 # Negative integers will take items from the last item backwards
-# You have to use either a For loop or List Comprehension to get items from a series of indices
+# You have to use either a For loop or List Comprehension to get 
+# items from a series of indices
 
 # The input port elements
 numbers = [ 0, 1, 2, 3, 4, 5, 6 7, 8, 9, 10 
 
 getItemAtIndex = numbers[ 3 ] # Getting the item at a specified index
 getLastItem = numbers[ -1 ] # Getting the last item in a list
-getLastItemPopMethod = numbers.pop() # Getting the last item in a list using the 'pop' method
+getLastItemPopMethod = numbers.pop() # Getting the last item in a list 
+# using the 'pop' method
 
 # The out port using our sublist queries
 OUT = getItemAtIndex, getLastItem, getLastItemPopMethod
