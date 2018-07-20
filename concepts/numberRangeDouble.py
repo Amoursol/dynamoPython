c8fe16b2a266b53d4813fbd91d20392581ef740e
Sol Amour <amoursol@gmail.com>
2018-07-20 22:30:22 +0100

    Update numberRangeDouble.py

--- concepts/numberRangeDouble.py
+++ concepts/numberRangeDouble.py
@@ -6,7 +6,8 @@ __twitter__ = '@solamour'
 __version__ = '1.0.0'
 
 # DEFINITION:
-# Custom definition to build a function similar to our DesignScript float range
+# Custom definition to build a function similar to our DesignScript 
+# float range
 def floatRange( start, end, step ):
 	for number in xrange( end ):
 		yield start
@@ -15,10 +16,13 @@ def floatRange( start, end, step ):
 # SYNTAX: floatRange( [start], stop[, step ] )
 # Start = Starting number of the sequence [Open]
 # Stop = Generate numbers up to, but not including this number [Closed]
-# Step = Difference between each number in the sequence. In order to pair with our DesignScript variant, we need to run this as: ( 1.0 / end)
+# Step = Difference between each number in the sequence. In order to pair 
+# with our DesignScript variant, we need to run this as: ( 1.0 / end)
 
 # NOTES: 
-# If we wish to use floating values (Doubles) we have to specify, hence in our step value we use 1.0 instead of 1. IF we used 1 (An integer) we would simply return a list of zeroes
+# If we wish to use floating values (Doubles) we have to specify, hence 
+# in our step value we use 1.0 instead of 1. IF we used 1 (An integer) 
+# we would simply return a list of zeroes
 
 # The input ports
 start = IN[0] # A number such as 0 (int)
