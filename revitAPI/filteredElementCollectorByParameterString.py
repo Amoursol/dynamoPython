9622b47fb3aac9a8ee1cee19a285d96a77d38420
Adam Bear <32963786+adambear82@users.noreply.github.com>
2018-07-24 12:27:57 +0100

    Update filteredElementCollectorByParameterString.py

--- revitAPI/filteredElementCollectorByParameterString.py
+++ revitAPI/filteredElementCollectorByParameterString.py
@@ -10,15 +10,20 @@ __version__ = '1.0.0'
 import clr
 # Adding the RevitServices.dll special Dynamo module to deal with Revit
 clr.AddReference('RevitServices')
-import RevitServices # Importing RevitServices
+import RevitServices  # Importing RevitServices
 # From RevitServices import the Document Manager
 from RevitServices.Persistence import DocumentManager
 # Adding the RevitAPI.dll module to access the Revit API
 clr.AddReference('RevitAPI')
-import Autodesk # Here we import the Autodesk namespace
+import Autodesk  # Here we import the Autodesk namespace
 # From the Autodesk namespace - derived down to the Revit Database
 # we import only the required classes
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInParameter, ParameterValueProvider, ElementId, FilterStringBeginsWith, FilterStringContains, FilterStringEndsWith, FilterStringEquals, FilterStringGreater, FilterStringGreaterOrEqual, FilterStringLess, FilterStringLessOrEqual, FilterStringRule, ElementParameterFilter
+from Autodesk.Revit.DB import \
+FilteredElementCollector, BuiltInParameter, \
+ParameterValueProvider, ElementId, FilterStringBeginsWith, \
+FilterStringContains, FilterStringEndsWith, FilterStringEquals, \
+FilterStringGreater, FilterStringGreaterOrEqual, FilterStringLess, \
+FilterStringLessOrEqual, FilterStringRule, ElementParameterFilter
 # to import all classes use the wildcard '*'
 # this will marginally slow down dynamo/python and may cause errors if
 # multiple classes are identically named
@@ -40,7 +45,7 @@ param = BuiltInParameter.ALL_MODEL_MARK
 # Once we have our BuiltInParameter, we need to get it's Element Id
 # and convert it to a Parameter Value Provider
 # in order to use it inside of our filter
-provider = ParameterValueProvider( ElementId( param ) )
+provider = ParameterValueProvider(ElementId(param))
 
 # refer to api docs for available evaulators
 # http://www.revitapidocs.com/2016/b317951e-6324-fc45-5860-6b616534d1ce.htm?query=filter%20string%20class
@@ -69,13 +74,14 @@ caseSensitive = False
 # http://www.revitapidocs.com/2015/056fd8e1-a989-b262-8c8e-9e79ece62b01.htm
 rule = FilterStringRule(provider, evaluator, test, caseSensitive)
 
-# generate a filter based off our Rule 
-filter = ElementParameterFilter( rule )
+# generate a filter based off our Rule
+filter = ElementParameterFilter(rule)
 
-# Now we have a valid rule to run against our Filtered Element Collector. # So in this case we pull everything inside the document
+# Now we have a valid rule to run against our Filtered Element Collector.
+# # So in this case we pull everything inside the document
 # but only if it passes our filter test
 # then make sure we return the elements themselves with '.ToElements()'
-analyticalCollector = fec( doc ).WherePasses( filter ).ToElements()
+analyticalCollector = fec(doc).WherePasses(filter).ToElements()
 
 # To get our results back inside of Dynamo
 # we need to append a list to the OUT port
