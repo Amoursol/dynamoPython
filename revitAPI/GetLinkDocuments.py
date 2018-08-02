9cc0e7fa25e7a71a5e0c82c621bf8a0e808c8ccd
Min Naung <p.jean90@gmail.com>
2018-08-02 09:42:04 +0800

    updated GetLinkDocuments.py
    i need "RevitServices" to instantiate current document (line-25).

--- revitAPI/GetLinkDocuments.py
+++ revitAPI/GetLinkDocuments.py
@@ -22,10 +22,10 @@ from Autodesk.Revit.DB import FilteredElementCollector, RevitLinkInstance
 from RevitServices.Persistence import DocumentManager
 
 # instantiate current document
-doc = DocumentManager.Instance.CurrentDBDocument;
+doc = DocumentManager.Instance.CurrentDBDocument
 
 # collect link documents from current document
-link_docs = FilteredElementCollector(doc).OfClass(RevitLinkInstance);
+link_docs = FilteredElementCollector(doc).OfClass(RevitLinkInstance)
 
 # return ["link document name","link document"] list-structure for each link documents
-OUT = [[i.Name.split(":")[0],i.GetLinkDocument()] for i in link_docs];
+OUT = [[i.Name.split(":")[0],i.GetLinkDocument()] for i in link_docs]
