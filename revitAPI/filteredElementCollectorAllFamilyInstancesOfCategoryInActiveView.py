ee63b9aed0cc42f244de4a23e156eaaaca6695a5
Sol Amour <amoursol@gmail.com>
2023-03-29 09:00:19 -0400

    Update filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py

--- revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
+++ revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
@@ -17,8 +17,11 @@ clr.AddReference("RevitAPI") # Adding the RevitAPI.dll module to access
 # the Revit API
 import Autodesk # Here we import the Autodesk namespace
 # From the Autodesk namespace - derived down to the Revit Database, we 
-# import only the Filtered Element Collector and BuiltInCategory classes
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, FamilyInstance
+# import only the Filtered Element Collector and BuiltInCategory classes.
+# The use of \ allows us to return a chained function on a new line without
+# throwing an error.
+from Autodesk.Revit.DB import FilteredElementCollector, \
+BuiltInCategory, FamilyInstance
 
 # Here we give the Revit Document a nickname of 'doc' which allows us to 
 # simply call 'doc' later without having to type the long namespace name 
@@ -28,7 +31,13 @@ doc = DocumentManager.Instance.CurrentDBDocument
 # 'Active View' of our document, all 'Family Instances' of a particular 
 # category ('Planting'). We then cast it to Elements so we can use it in 
 # Dynamo.
-builtInCollector = FilteredElementCollector(doc, doc.ActiveView.Id).OfClass(FamilyInstance).OfCategory(BuiltInCategory.OST_Planting).WhereElementIsNotElementType().ToElements()
+# The use of \ allows us to return a chained function on a new line without
+# throwing an error.
+builtInCollector = FilteredElementCollector(doc, doc.ActiveView.Id) \
+    .OfClass(FamilyInstance) \
+    .OfCategory(BuiltInCategory.OST_Planting) \
+    .WhereElementIsNotElementType() \
+    .ToElements()
 
 # To get our results back inside of Dynamo, we need to append a list to 
 # the OUT port
