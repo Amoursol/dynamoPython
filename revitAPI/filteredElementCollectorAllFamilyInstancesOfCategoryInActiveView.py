323692537f2757a3c2f33e815c74412b7ce171a1
Sol Amour <amoursol@gmail.com>
2023-03-29 08:56:19 -0400

    Update filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py

--- revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
+++ revitAPI/filteredElementCollectorAllFamilyInstancesOfCategoryInActiveView.py
@@ -18,8 +18,7 @@ clr.AddReference("RevitAPI") # Adding the RevitAPI.dll module to access
 import Autodesk # Here we import the Autodesk namespace
 # From the Autodesk namespace - derived down to the Revit Database, we 
 # import only the Filtered Element Collector and BuiltInCategory classes
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, 
-FamilyInstance
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, FamilyInstance
 
 # Here we give the Revit Document a nickname of 'doc' which allows us to 
 # simply call 'doc' later without having to type the long namespace name 
@@ -29,7 +28,7 @@ doc = DocumentManager.Instance.CurrentDBDocument
 # 'Active View' of our document, all 'Family Instances' of a particular 
 # category ('Planting'). We then cast it to Elements so we can use it in 
 # Dynamo.
-builtInCollector = FilteredElementCollector( doc, doc.ActiveView.Id ).OfClass( FamilyInstance ).OfCategory( BuiltInCategory.OST_Planting ).WhereElementIsNotElementType().ToElements()
+builtInCollector = FilteredElementCollector(doc, doc.ActiveView.Id).OfClass(FamilyInstance).OfCategory(BuiltInCategory.OST_Planting).WhereElementIsNotElementType().ToElements()
 
 # To get our results back inside of Dynamo, we need to append a list to 
 # the OUT port
